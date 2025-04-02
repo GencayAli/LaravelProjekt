0442a4278a576a3bc59f594f7b1b231ab6915c00
Mehmet Ali Gencay <gencayali20@gmail.com>
2025-04-02 14:11:56 +0200

    Weiter hinzufügen

--- laravel-start/database/factories/SongFactory.php
+++ laravel-start/database/factories/SongFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Factories\Factory;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Song>
