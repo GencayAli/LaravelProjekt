04ff20fa3700bada576a2f22ca6afe4457da26c0
Mehmet Ali Gencay <gencayali20@gmail.com>
2025-04-02 15:00:59 +0200

    neue

--- laravel-rest/app/Models/Song.php
+++ laravel-rest/app/Models/Song.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Song extends Model
 {
+    use HasFactroy;
     protected $fillable = [
-        
+        'title',
+        'band',
+        'labels_id_ref',
+
     ];
 }
