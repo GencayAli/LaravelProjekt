efd913db97d0a4e0414093deb25be7b96a739896
Mehmet Ali Gencay <gencayali20@gmail.com>
2025-04-04 11:04:14 +0200

    ....

--- laravel-rest/app/Http/Controllers/LabelsController.php
+++ laravel-rest/app/Http/Controllers/LabelsController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Label;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 
 class LabelsController extends Controller
